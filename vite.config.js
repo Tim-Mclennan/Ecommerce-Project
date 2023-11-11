0aa8d98e9b84ff89b58e8910d4fac59ce8b1ac73
Tim <timmymclennan@gmail.com>
2023-11-11 11:30:51 +1100

    using a ternary operator to check if the NODE_ENV environment variable is set to 'production'. If it is, we set the base property to '/Ecommerce-Project/', and if it's not (which means you're in development mode), we set it to '/'.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/Ecommerce-Project/' : '/'
 })
+
